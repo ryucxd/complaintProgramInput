aeeb730b56682df7badb4844f762c31f6c629bf3
ryucxd <ryucxd@gmail.com>
2020-08-18 10:02:15 +0100

    Add project files.

---
+++ complaintProgramInput/frmLogin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace complaintProgramInput
+{
+    public partial class frmLogin : Form
+    {
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void txtPassword_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                btnLogin.PerformClick();
+        }
+
+        private void txtUsername_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                txtPassword.Focus();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            session sessionLogin = new session();
+            sessionLogin.login(txtUsername.Text, txtPassword.Text);
+            if (sessionLogin.passwordWrong == true)
+            {
+                MessageBox.Show("Wrong username/password!");
+                wipeTxt(2);
+                return;
+            }
+            else //its a true login
+                MessageBox.Show("yes");
+            
+        }
+
+        private void wipeTxt(int num)
+        {
+            if (num == 2) //only wipe pass
+                txtPassword.Text = "";
+        }
+
+    }
+}
---
+++ complaintProgramInput/session.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace complaintProgramInput
+{
+    class session
+    {
+        public string username { get; set; }
+        public string password { get; set; }
+        public string ID { get; set; }
+
+        public bool passwordWrong { get; set; }
+
+        //capture login for user  THEY HAVE TO HAVE /IS ENGINEER/ ENABLED
+        public void login(string _username, string _password)
+        {
+            username = _username;
+            password = _password;
+            check_login();
+        }
+
+        private void check_login()
+        {
+            string sql = " ";
+            using (SqlConnection conn = new SqlConnection(CONNECT.ConnectionStringUser))
+            {
+                //first check if the password or username is correct
+                sql = "SELECT COALESCE(id,0) FROM dbo.[user] WHERE username = '" + username + "' AND password = '" + password + "';";
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    conn.Open();
+                    ID = Convert.ToString(cmd.ExecuteScalar());
+                    conn.Close();
+                    if (ID == "0" || string.IsNullOrEmpty(ID))
+                    {
+                        passwordWrong = true;
+                        return;
+                    }
+                }
+
+            }
+        }
+    }
+}
